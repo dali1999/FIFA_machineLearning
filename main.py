fed02ab8de09a86db21ee22a0ea7752f91e90d0c
stakbucks <dhassidu11@gmail.com>
2022-11-11 16:34:28 +0900

    data

---
+++ main.py
@@ -0,0 +1,7 @@
+from numpy import genfromtxt
+stats = genfromtxt('stats.csv', delimiter=',', encoding='utf-8-sig')
+print(stats)
+print(stats.shape)
+overall = genfromtxt('overall.csv', delimiter=',', encoding='utf-8-sig', dtype=int)
+print(overall)
+print(overall.shape)
